e440d86442419bbc27d5b73f2704dead640e52bf
Anton <antonshvetswork@yandex.ru>
2022-05-24 23:26:16 +0300

    Выполнены задания 1-4

--- ajax js/scripts.js
+++ ajax js/scripts.js
@@ -2,15 +2,39 @@ const loadBtn = document.querySelector(".js-load");
 const resultsContainer = document.querySelector(".js-results");
 const searchInput = document.querySelector(".js-input");
 
+let ets = "https://api.github.com/users/Anton-Shvetc";
 loadBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
   const searchValue = searchInput.value.trim().toLowerCase();
-  fetch(`https://api.github.com/users/${searchValue}`).then(
-    (data) =>
-      (resultsContainer.innerHTML = `<div class="response-container">
+
+  axiosData(`https://api.github.com/users/${searchValue}`);
+  fetch(`https://api.github.com/users/${searchValue}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      resultsContainer.innerHTML = `<div class="response-container">
                 <img src="${data.avatar_url}">
                 <p> Имя: <span>${data.name}</span><p>
                 <p> О себе: <span>${data.bio}</span><p>
                 <p> Кол-во репозиториев: <span>${data.public_repos}</span><p>
-            </div>`)
-  );
+            </div>`;
+    });
 });
+
+function axiosData(url) {
+  return axios.get(url).then((response) => {
+    console.log(response.data);
+    response.data.map((e) => {
+      newDiv = document.createElement("div");
+      newDiv.classList.add("response-container");
+      resultsContainer.appendChild(newDiv);
+      newDiv.innerHTML = `
+                     <p> id: <span>${e.id}</span><p>
+                    <p> title: <span>${e.title}</span><p>
+                    <p> body: <span>${e.body}</span><p>
+                `;
+    });
+  });
+}
+axiosData(`https://jsonplaceholder.typicode.com/posts`);
